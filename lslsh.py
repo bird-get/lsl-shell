c12df307c3029f6abb413bde70802fc1b4f88aac
Bridget <bridget.littlebird@pm.me>
2020-04-12 21:53:32 +0200

    Refactor

--- lslsh.py
+++ lslsh.py
@@ -7,84 +7,83 @@ from typing import Dict
 import readline
 import cmd
 
-
-class Shell(cmd.Cmd):
-    prompt = "> "
-
-    def get_names(self):
-        return dir(self)
-
 SECRET_KEY: str = "29731e5170353a8b235098c43cd2099a4e805c55fb4395890e81f437c17334a9"
 
 warnings.filterwarnings("ignore", category=InsecureRequestWarning)
 
-def send_cmd(url: str, cmd: str) -> Dict:
-    data = {"secret_key": SECRET_KEY,
-            "command": cmd}
+class Shell(cmd.Cmd):
+    prompt = "> "
+    url = None
 
-    try:
-        response = requests.post(url, json=data, verify=False)
-        response_data = response.json()
-    except (requests.ConnectionError, requests.exceptions.MissingSchema,
-            requests.exceptions.InvalidURL):
-        raise requests.exceptions.InvalidURL
-    except JSONDecodeError:
-        raise Exception("Error: Response has malformed json")
+    def get_names(self):
+        return dir(self)
 
-    error = response_data.get("error", None)
-    if error:
-        raise Exception(f"Error: {error}")
+    def precmd(self, line):
+        if line == "EOF":
+            return "exit"
+        return line
 
-    return response_data
-
-def init() -> str:
-    initialized: bool = False
-    while not initialized:
-        url = input("url > ")
+    def send_cmd(self, url: str, cmd: str) -> Dict:
+        data = {"secret_key": SECRET_KEY,
+                "command": cmd}
+    
         try:
-            result = send_cmd(url, "init")
+            response = requests.post(url, json=data, verify=False)
+            response_data = response.json()
+        except (requests.ConnectionError, requests.exceptions.MissingSchema,
+                requests.exceptions.InvalidURL):
+            raise requests.exceptions.InvalidURL
+        except JSONDecodeError:
+            raise Exception("Error: Response has malformed json")
+    
+        error = response_data.get("error", None)
+        if error:
+            raise Exception(f"Error: {error}")
+    
+        return response_data
+
+    def do_connect(self, url):
+        """Connect to given URL."""
+        try:
+            result = self.send_cmd(url, "init")
         except requests.exceptions.InvalidURL:
             print("Error: Invalid URL")
-            continue
-
+            return
+    
         uuid = result.get("uuid", None)
         if not uuid:
-            raise Exception("Error: Invalid response")
+            print("Error: Invalid response")
+            return
+    
+        print(f"Connected to {uuid}")
+        print("_______________________________________________________________________________")
+        print("")
+        self.url = url
 
-        initialized = True
+    def do_exit(self, arg):
+        """Exit the shell."""
+        print("")
+        if self.url:
+            result = self.send_cmd(self.url, "disconnect")
+            if result.get("result") == "disconnected":
+                print("Disconnected from remote.")
+            else:
+                print("Disconnected from remote (without acknowledgement).")
 
-    print(f"Connected to {uuid}")
-    print("_______________________________________________________________________________")
-    print("")
-    return url
+        return True
+    
+    def add_cmd(self, name, help_text):
+        def do_cmd(arg):
+            result = self.send_cmd(self.url, arg)
+            print(result)
+    
+        do_cmd.__doc__ = help_text
+        do_cmd.__name__ = name
+    
+        setattr(self, f"do_{name}", do_cmd)
 
-def disconnect(url: str):
-    result = send_cmd(url, "disconnect")
-    print("")
-    if result.get("result") == "disconnected":
-        print("Disconnected from remote.")
-    else:
-        print("Disconnected from remote (without acknowledgement).")
+def run():
+    shell = Shell()
+    shell.cmdloop()
 
-def add_cmd(cls, name, help_text):
-    def do_cmd(arg):
-        result = send_cmd(cls.url, arg)
-        print(result)
-
-    do_cmd.__doc__ = help_text
-    do_cmd.__name__ = name
-
-    setattr(cls, f"do_{name}", do_cmd)
-
-def run(url: str):
-    cmd = Shell()
-    cmd.url = url
-    add_cmd(cmd, "exit", "Disconnect from remote.")
-    cmd.cmdloop()
-
-try:
-    url = init()
-    run(url)
-except EOFError:
-    disconnect(url)
-    pass
+run()
